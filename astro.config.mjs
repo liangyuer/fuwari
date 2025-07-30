4401c47777ff9ede9e195317bd5b1c1c546ed643
liangyuer <2752237525@qq.com>
2025-07-30 11:21:47 +0800

    update10

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://0721255.xyz/",
+	site: "https://www.0721255.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
