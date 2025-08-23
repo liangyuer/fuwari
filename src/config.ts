50e9bd37d5e0cb6d711a034e845b4671b7d053f0
liangyuer <2752237525@qq.com>
2025-08-23 14:13:06 +0800

    update60

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "网盘",
-			url: "https://alist.ciallo.autos", // Internal links should not include the base path, as it is automatically added
+			url: "https://pan.0721255.xyz", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 		{
